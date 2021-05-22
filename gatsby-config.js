5373785067362c4c68960fbf117e043e6396637f
Han Dao <hanvdao@gmail.com>
2021-05-22 01:01:38 -0500

    changed title in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Han Dao`,
+    description: `CS Undergraduate @ Stanford`,
     author: `@gatsbyjs`,
   },
   plugins: [
